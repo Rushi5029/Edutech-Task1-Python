9a4e8e6f062e17b40bb4b5013e211ee16b654d22
Rushi5029 <151387596+Rushi5029@users.noreply.github.com>
2026-04-24 11:28:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello Sir")
+
+name = "Rushi"
+age = 21
+computer_language = "Python"
+version = 3.10
+
+print("My Name Is", name)
+print("I am", age, "Year Old")
+print("I am Learing", computer_language, version)
